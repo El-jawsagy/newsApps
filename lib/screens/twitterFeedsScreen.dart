1318efca172a3fcd6fa854aec6348675ce538260
mahmo <mahmoudragab0809@gmail.com>
2019-07-08 00:41:11 +0200

    link twitter feeds with api need to make status stable

--- lib/screens/twitterFeedsScreen.dart
+++ lib/screens/twitterFeedsScreen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/apis/post_Api.dart';
+import 'package:flutter_app/models/post.dart';
 import 'package:flutter_app/shared_ui/drawerNavigation.dart';
+import 'package:flutter_app/utilities/shared_Method.dart';
 
 class twitterFeeds extends StatefulWidget {
   @override
   _twitterFeedsState createState() => _twitterFeedsState();
 }
-List<int> ids =[];
+
+PostApi postApi = PostApi();
+List<int> ids = [];
+List<int> bool =[];
+
+
 class _twitterFeedsState extends State<twitterFeeds> {
   @override
   void initState() {
     super.initState();
-    ids=[2,5,3];
+    ids = [2, 5, 3];
+    bool =[];
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,29 +35,57 @@ class _twitterFeedsState extends State<twitterFeeds> {
         ],
       ),
       drawer: DrawerNavigation(),
-      body: ListView.builder(padding: EdgeInsets.all(8.0),
-        itemBuilder: (context, position) {
-          return Padding(
-            padding: const EdgeInsets.only(bottom:8.0),
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 16.0,
-                  right: 16,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    _rowTitle(),
-                    _rowCenter(),
-                    _drawLine(),
-                    _rowBottom(position,ids),
-                  ],
-                ),
-              ),
-            ),
-          );
+      body: FutureBuilder(
+        future: postApi.fetchPost("1"),
+        builder: (context, AsyncSnapshot snapShot) {
+          switch (snapShot.connectionState) {
+            case ConnectionState.none:
+              return connectionError();
+              break;
+            case ConnectionState.waiting:
+              return loading();
+              break;
+            case ConnectionState.active:
+              return loading();
+              break;
+            case ConnectionState.done:
+              if (snapShot.hasError) {
+                return error(snapShot.error);
+              } else {
+                if (snapShot.hasData) {
+                  List<post> posts = snapShot.data;
+                  return ListView.builder(
+                    padding: EdgeInsets.all(8.0),
+                    itemBuilder: (context, position) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              left: 16.0,
+                              right: 16,
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                _rowTitle(),
+                                _rowCenter(posts[position]),
+                                _drawLine(),
+                                _rowBottom(posts[position], ids),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: posts.length,
+                  );
+                } else {
+                  return noData();
+                }
+              }
+              break;
+          }
         },
-        itemCount: 20,
       ),
     );
   }
@@ -72,11 +110,11 @@ class _twitterFeedsState extends State<twitterFeeds> {
                 child: Row(
                   children: <Widget>[
                     Padding(
-                      padding: const EdgeInsets.only(right:4.0),
+                      padding: const EdgeInsets.only(right: 4.0),
                       child: Text(
                         "Mahmoud Ragab",
-                        style:
-                            TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
                       ),
                     ),
                     Text(
@@ -90,7 +128,12 @@ class _twitterFeedsState extends State<twitterFeeds> {
                 ),
               ),
               Row(
-                children: <Widget>[Text("Fri,12 May 2017 - 14.30",style: TextStyle(color: Colors.grey.shade500),)],
+                children: <Widget>[
+                  Text(
+                    "Fri,12 May 2017 - 14.30",
+                    style: TextStyle(color: Colors.grey.shade500),
+                  )
+                ],
               ),
             ],
           )
@@ -99,7 +142,7 @@ class _twitterFeedsState extends State<twitterFeeds> {
     );
   }
 
-  Widget _rowBottom(int posi,List<int> ids) {
+  Widget _rowBottom(post tempPost, List<int> ids) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
@@ -108,16 +151,17 @@ class _twitterFeedsState extends State<twitterFeeds> {
             IconButton(
                 icon: Icon(
                   Icons.repeat,
-                  color: (ids.contains(posi))?Colors.orange : Colors.grey.shade500,
+                  color: (ids.contains(tempPost.id))
+                      ? Colors.orange
+                      : Colors.grey.shade500,
                 ),
                 onPressed: () {
-                  if(ids.contains(posi)){
-                    ids.remove(posi);
-                  }else{
-                    ids.add(posi);
+                  if (ids.contains(tempPost.id)) {
+                    ids.remove(tempPost.id);
+                  } else {
+                    ids.add(tempPost.id);
                   }
-                  setState(() {
-                  });
+                  setState(() {});
                 }),
             Text(
               "25",
@@ -145,20 +189,52 @@ class _twitterFeedsState extends State<twitterFeeds> {
     );
   }
 
-  Widget _rowCenter() {
+  Widget _rowCenter(post tempPost) {
+    String tempString = tempPost.content.substring(0, 78);
+    String readMore = "...Read More";
+    String ReadLess ="...Read Less";
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
-      child: Text(
-        "we also talk about the future of work as the robots advance, and we ask retro phone",
-        style: TextStyle(wordSpacing: 1.25, fontSize: 16,height: 1.2,),
+      child: Wrap(
+        children: <Widget>[
+          Text(
+            (bool.contains(tempPost.id)) ? tempPost.content:tempString,
+            style: TextStyle(
+              wordSpacing: 1.25,
+              fontSize: 16,
+              height: 1.2,
+            ),
+          ),
+          InkWell(
+              onTap: () {
+
+                if (bool.contains(tempPost.id)) {
+                  bool.remove(tempPost.id);
+                  ;
+                } else {
+                  bool.add(tempPost.id);
+                  ;
+                }
+                setState(() {
+
+                });
+
+
+
+              },
+              child: Text(
+                (bool.contains(tempPost.id)?ReadLess:readMore),
+                style: TextStyle(color: Colors.cyan),
+              )),
+        ],
       ),
     );
   }
 
- Widget _drawLine() {
-    return  Container(
+  Widget _drawLine() {
+    return Container(
       color: Colors.grey.shade200,
       height: 1,
     );
- }
+  }
 }
